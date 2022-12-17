95b8be13f9c760a6d6b82c62fe5431e23a31cbc5
EC2 Default User <ec2-user@ip-172-31-45-250.ec2.internal>
2022-12-17 11:04:33 +0000

    update

--- Gemfile
+++ Gemfile
@@ -63,6 +63,7 @@ gem 'dotenv-rails'
 group :production do
   gem 'mysql2'
 end
+
 gem "net-smtp"
 gem "net-pop"
 gem "net-imap"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -96,6 +97,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector

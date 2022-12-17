9acfe81342952630687a649e46745ea025bdd90f
EC2 Default User <ec2-user@ip-172-31-45-250.ec2.internal>
2022-12-17 11:24:02 +0000

    update

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require "action_mailer/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,8 +11,8 @@ module FurugiMeguri
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    config.autoload_paths << "#{config.root}/lib"
-    config.eager_load_paths << "#{config.root}/lib"
+    # config.autoload_paths << "#{config.root}/lib"
+    # config.eager_load_paths << "#{config.root}/lib"
 
     # Configuration for the application, engines, and railties goes here.
     #

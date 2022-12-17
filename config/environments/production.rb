05bd07c64eb1f73bf4747a851afa1137e4b9f161
EC2 Default User <ec2-user@ip-172-31-45-250.ec2.internal>
2022-12-17 11:11:43 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,10 +13,10 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  config.eager_load_paths += %W(#{config.root}/app/lib)
+  # config.eager_load_paths += %W(#{config.root}/app/lib)
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
